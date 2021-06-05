0d4fcae0316d883e15006960535033ea1b97dafc
rayudu <mh.rayudu668@gmail.com>
2021-06-06 03:13:59 +0530

    second commit

--- src/test/java/jai/hanuman/MavenJava/RestAPITest.java
+++ src/test/java/jai/hanuman/MavenJava/RestAPITest.java
@@ -7,6 +7,7 @@ public class RestAPITest {
 	@Test
 	public void RestAPILogin() {
 		System.out.println("User should login to Rest API");
+		System.out.println("Code to gitone");
 	}
 	@Test
 	public void launchRestAPIBrowser() {
